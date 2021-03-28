a78326ed771ab70b97010c507c92bfeb3d032243
Matthew Rogers <matthew.rogers@cloudreach.com>
2021-03-28 12:23:29 +0100

    Move K8S services outside of main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,14 @@
 ## Create a new EKS Cluster, along with supporting infrastructure
 
 locals {
-  region = "us-west-2"
+  region = "eu-west-2"
   role_arn = "arn:aws:iam::772290144575:role/Admin"
   profile = "cr-lab"
+  current_ip = ["88.98.247.243/32"]
 }
 
 module "eks-cluster" {
   source = "./modules/eks-cluster"
   region = local.region
+  api_access_CIDR_range = local.current_ip #The address of the NAT gateway will be added automatically.
 }
-
-module "build-example-webapp" {
-  source = "./modules/ecr-image"
-  build_folder = "../webapp"
-  app_name = "example-webapp"
-  tag = "0.0.1"
-  region = local.region
-  profile = "cr-lab-mr"
-}
-
-# resource "kubernetes_service" "example-webapp" {
-#   metadata {
-#     name = "example-webapp"
-#   }
-#   spec {
-#     type = "LoadBalancer"
-#     port {
-#       port = 80
-#       target_port = 3000
-#     }
-#     selector {
-#       app = "example_webapp"
-#     }
-#   }
-# }
-
-# resource "kubernetes_pod" "example-webapp" {
-#   metadata {
-#     name = "example-webapp"
-#     labels {
-#       app = "example-webapp"
-#     }
-#     spec {
-#       container {
-#
-#       }
-#     }
-#   }
-# }
-
-# module "launch-example-webapp" {
-#   source = "./modules/kubernetes"
-#   pod_file = "./k8s/example-webapp/example-webapp.pod.yaml"
-#   service_file = "./k8s/example-webapp/example-webapp.service.yaml"
-# }
